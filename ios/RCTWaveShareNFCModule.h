5644fddc5b672fc7ebaef914051b64b57d67b075
sachinrohit <sachin.rohit@live.com>
2023-01-08 17:51:04 +0530

    added rn-native modules

---
+++ ios/RCTWaveShareNFCModule.h
@@ -0,0 +1,12 @@
+//
+//  RCTWaveShareNFCModule.h
+//  NFCTestApp
+//
+//  Created by sachin rohit on 08/01/23.
+//
+
+#ifndef RCTWaveShareNFCModule_h
+#define RCTWaveShareNFCModule_h
+
+
+#endif /* RCTWaveShareNFCModule_h */
---
+++ ios/RCTWaveShareNFCModule.m
@@ -0,0 +1,8 @@
+//
+//  RCTWaveShareNFCModule.m
+//  NFCTestApp
+//
+//  Created by sachin rohit on 08/01/23.
+//
+
+#import <Foundation/Foundation.h>
