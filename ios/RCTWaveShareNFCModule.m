406ae4f069e09ff62c89eb8e2569935efec893e2
sachinrohit <sachin.rohit@live.com>
2023-03-31 02:13:40 +0530

    refactor iOS plugin

--- ios/RCTWaveShareNFCModule.m
+++ ios/RCTWaveShareNFCModule.m
@@ -52,7 +52,9 @@ RCT_EXPORT_METHOD(sendImageEvent:(NSString *)imageUrl location:(NSString *)locat
     NSLog(@"%@", imageFromURL);
 //    [[NFCTagReader shared] sendImage:imageFromURL password:nil einkSizeType:EInkSizeType420];
 //
-    [self fetchImageFromServer];
+    // NonParameters [self fetchImageFromServer];
+    
+    [self fetchImageFromServer:(NSString *)imageUrl accessToken:location];
 
     NSLog(@"Downloading completed... %f", imageFromURL.size.width);
   }
@@ -77,7 +79,7 @@ RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(getName)
 ////    // }
 //}
 
-- (void)fetchImageFromServer {
+- (void)fetchImageFromServerNonParameters {
   
   // [[NFCTagReader shared] getUID];
   dispatch_semaphore_t sema = dispatch_semaphore_create(0);
@@ -124,4 +126,58 @@ RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(getName)
   NSLog(@"got response data 2");
 
 }
+
+// Sachin - New method
+- (void)fetchImageFromServer : (NSString *)imageUrl accessToken:(NSString *)token {
+  dispatch_semaphore_t sema = dispatch_semaphore_create(0);
+
+//  NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"https://api.staging2.rucmonkey.co.nz/ruc/v3/myruc/vehicle/ruclicense/31412/label/bitmap"]
+//    cachePolicy:NSURLRequestUseProtocolCachePolicy
+//    timeoutInterval:10.0];
+//
+  NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:imageUrl]
+    cachePolicy:NSURLRequestUseProtocolCachePolicy
+    timeoutInterval:10.0];
+  
+  NSString *formatHeadersForURL = [NSString stringWithFormat:@"Bearer %@", token];
+  
+//  NSDictionary *headers = @{
+//    @"Authorization": @"Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiIxODYiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9lbWFpbGFkZHJlc3MiOiJteXJ1Y2RldkBwaWNvYnl0ZS5jby5ueiIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL3NpZCI6IjIiLCJuYmYiOjE2ODAyMDA2NjksImV4cCI6MTY4MDIxNTA2OSwiaXNzIjoiaHR0cHM6Ly93d3cucnVjbW9ua2V5LmNvLm56In0.qosm32GUyaJzOyjJyV1MGG2irp1T1Ixxlikv1p3t1VR7cSneb1UUk8nFZpWPduPUmwpID7Sbs9BPRno56VXWEA"
+//  };
+  
+  NSDictionary *headers = @{
+    @"Authorization": formatHeadersForURL
+  };
+
+  [request setAllHTTPHeaderFields:headers];
+  [request setHTTPMethod:@"GET"];
+
+  NSURLSession *session = [NSURLSession sharedSession];
+  NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request
+  completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+    if (error) {
+      NSLog(@"%@", error);
+      NSLog(@"Error ");
+      dispatch_semaphore_signal(sema);
+    } else {
+      NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *) response;
+      NSError *parseError = nil;
+      
+      // unused, save memory - UIImage* downloadimage = [UIImage imageWithData:data];
+      UIImage* image = [[UIImage alloc] initWithData:data];
+      
+      NSLog(@"received data from server %@",image);
+    
+      [[NFCTagReader shared] sendImage:image password:NULL einkSizeType:EInkSizeType420];
+      
+      dispatch_semaphore_signal(sema);
+      NSLog(@"got response data 3");
+    }
+  }];
+  [dataTask resume];
+  dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER);
+  NSLog(@"got response data 2");
+
+}
+
 @end
